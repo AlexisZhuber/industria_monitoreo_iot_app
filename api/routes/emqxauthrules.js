ebd623fac16cde6789bdab80c9893b18acd660d4
AlexisMora602 <alexismora602@gmail.com>
2022-10-12 20:09:52 -0500

    commit

---
+++ api/routes/emqxauthrules.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+//models import
+import Rule from "../models/usermqtt.js";
+
+router.post("/rule", async (req, res) => {
+  try {
+    const name = req.body.username;
+    const password = req.body.password;
+
+    const newRule = {
+      username: name,
+      password: password
+    };
+
+    const emqxauthrule = await Rule.create(newRule);
+
+    console.log(emqxauthrule);
+
+    const toSend = {
+      status: "success",
+    };
+
+    res.json(toSend);
+  } catch (error) {
+    console.log("ERROR - REGISTER ENDPOINT");
+    console.log(error);
+
+    const toSend = {
+      status: "error",
+      error: error,
+    };
+
+    res.status(500).json(toSend);
+  }
+});
+
+
+
+module.exports = router;
