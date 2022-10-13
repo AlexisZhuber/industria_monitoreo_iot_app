d92c45eb26f1c6918fc7a10b9defcef75ec52fda
AlexisMora602 <alexismora602@gmail.com>
2022-10-12 21:07:47 -0500

    commit

---
+++ .nuxt/components/index.js
@@ -0,0 +1,112 @@
+export const AnimatedNumber = () => import('../../components/AnimatedNumber.vue' /* webpackChunkName: "components/animated-number" */).then(c => wrapFunctional(c.default || c))
+export const Badge = () => import('../../components/Badge.vue' /* webpackChunkName: "components/badge" */).then(c => wrapFunctional(c.default || c))
+export const BaseAlert = () => import('../../components/BaseAlert.vue' /* webpackChunkName: "components/base-alert" */).then(c => wrapFunctional(c.default || c))
+export const BaseButton = () => import('../../components/BaseButton.vue' /* webpackChunkName: "components/base-button" */).then(c => wrapFunctional(c.default || c))
+export const BaseDropdown = () => import('../../components/BaseDropdown.vue' /* webpackChunkName: "components/base-dropdown" */).then(c => wrapFunctional(c.default || c))
+export const BasePagination = () => import('../../components/BasePagination.vue' /* webpackChunkName: "components/base-pagination" */).then(c => wrapFunctional(c.default || c))
+export const BaseProgress = () => import('../../components/BaseProgress.vue' /* webpackChunkName: "components/base-progress" */).then(c => wrapFunctional(c.default || c))
+export const BaseSwitch = () => import('../../components/BaseSwitch.vue' /* webpackChunkName: "components/base-switch" */).then(c => wrapFunctional(c.default || c))
+export const BaseTable = () => import('../../components/BaseTable.vue' /* webpackChunkName: "components/base-table" */).then(c => wrapFunctional(c.default || c))
+export const CloseButton = () => import('../../components/CloseButton.vue' /* webpackChunkName: "components/close-button" */).then(c => wrapFunctional(c.default || c))
+export const ImageUpload = () => import('../../components/ImageUpload.vue' /* webpackChunkName: "components/image-upload" */).then(c => wrapFunctional(c.default || c))
+export const Json = () => import('../../components/Json.vue' /* webpackChunkName: "components/json" */).then(c => wrapFunctional(c.default || c))
+export const LoadingPanel = () => import('../../components/LoadingPanel.vue' /* webpackChunkName: "components/loading-panel" */).then(c => wrapFunctional(c.default || c))
+export const Modal = () => import('../../components/Modal.vue' /* webpackChunkName: "components/modal" */).then(c => wrapFunctional(c.default || c))
+export const NavbarToggleButton = () => import('../../components/NavbarToggleButton.vue' /* webpackChunkName: "components/navbar-toggle-button" */).then(c => wrapFunctional(c.default || c))
+export const Slider = () => import('../../components/Slider.vue' /* webpackChunkName: "components/slider" */).then(c => wrapFunctional(c.default || c))
+export const Components = () => import('../../components/index.js' /* webpackChunkName: "components/components" */).then(c => wrapFunctional(c.default || c))
+export const Breadcrumb = () => import('../../components/Breadcrumb/Breadcrumb.vue' /* webpackChunkName: "components/breadcrumb" */).then(c => wrapFunctional(c.default || c))
+export const BreadcrumbItem = () => import('../../components/Breadcrumb/BreadcrumbItem.vue' /* webpackChunkName: "components/breadcrumb-item" */).then(c => wrapFunctional(c.default || c))
+export const RouteBreadcrumb = () => import('../../components/Breadcrumb/RouteBreadcrumb.vue' /* webpackChunkName: "components/route-breadcrumb" */).then(c => wrapFunctional(c.default || c))
+export const BarChart = () => import('../../components/Charts/BarChart.js' /* webpackChunkName: "components/bar-chart" */).then(c => wrapFunctional(c.default || c))
+export const LineChart = () => import('../../components/Charts/LineChart.js' /* webpackChunkName: "components/line-chart" */).then(c => wrapFunctional(c.default || c))
+export const PieChart = () => import('../../components/Charts/PieChart.js' /* webpackChunkName: "components/pie-chart" */).then(c => wrapFunctional(c.default || c))
+export const Config = () => import('../../components/Charts/config.js' /* webpackChunkName: "components/config" */).then(c => wrapFunctional(c.default || c))
+export const Utils = () => import('../../components/Charts/utils.js' /* webpackChunkName: "components/utils" */).then(c => wrapFunctional(c.default || c))
+export const Calendar = () => import('../../components/Calendar/Calendar.vue' /* webpackChunkName: "components/calendar" */).then(c => wrapFunctional(c.default || c))
+export const CalendarHeader = () => import('../../components/Calendar/CalendarHeader.vue' /* webpackChunkName: "components/calendar-header" */).then(c => wrapFunctional(c.default || c))
+export const Card = () => import('../../components/Cards/Card.vue' /* webpackChunkName: "components/card" */).then(c => wrapFunctional(c.default || c))
+export const StatsCard = () => import('../../components/Cards/StatsCard.vue' /* webpackChunkName: "components/stats-card" */).then(c => wrapFunctional(c.default || c))
+export const SweetAlertHeader = () => import('../../components/Headers/SweetAlertHeader.vue' /* webpackChunkName: "components/sweet-alert-header" */).then(c => wrapFunctional(c.default || c))
+export const CountryMapCard = () => import('../../components/Dashboard/CountryMapCard.vue' /* webpackChunkName: "components/country-map-card" */).then(c => wrapFunctional(c.default || c))
+export const TaskList = () => import('../../components/Dashboard/TaskList.vue' /* webpackChunkName: "components/task-list" */).then(c => wrapFunctional(c.default || c))
+export const UserTable = () => import('../../components/Dashboard/UserTable.vue' /* webpackChunkName: "components/user-table" */).then(c => wrapFunctional(c.default || c))
+export const Collapse = () => import('../../components/Collapse/Collapse.vue' /* webpackChunkName: "components/collapse" */).then(c => wrapFunctional(c.default || c))
+export const CollapseItem = () => import('../../components/Collapse/CollapseItem.vue' /* webpackChunkName: "components/collapse-item" */).then(c => wrapFunctional(c.default || c))
+export const ShoppingTable = () => import('../../components/ExtendedTables/ShoppingTable.vue' /* webpackChunkName: "components/shopping-table" */).then(c => wrapFunctional(c.default || c))
+export const Notification = () => import('../../components/NotificationPlugin/Notification.vue' /* webpackChunkName: "components/notification" */).then(c => wrapFunctional(c.default || c))
+export const Notifications = () => import('../../components/NotificationPlugin/Notifications.vue' /* webpackChunkName: "components/notifications" */).then(c => wrapFunctional(c.default || c))
+export const NotificationPlugin = () => import('../../components/NotificationPlugin/index.js' /* webpackChunkName: "components/notification-plugin" */).then(c => wrapFunctional(c.default || c))
+export const BaseCheckbox = () => import('../../components/Inputs/BaseCheckbox.vue' /* webpackChunkName: "components/base-checkbox" */).then(c => wrapFunctional(c.default || c))
+export const BaseInput = () => import('../../components/Inputs/BaseInput.vue' /* webpackChunkName: "components/base-input" */).then(c => wrapFunctional(c.default || c))
+export const BaseRadio = () => import('../../components/Inputs/BaseRadio.vue' /* webpackChunkName: "components/base-radio" */).then(c => wrapFunctional(c.default || c))
+export const IconCheckbox = () => import('../../components/Inputs/IconCheckbox.vue' /* webpackChunkName: "components/icon-checkbox" */).then(c => wrapFunctional(c.default || c))
+export const TagsInput = () => import('../../components/Inputs/TagsInput.vue' /* webpackChunkName: "components/tags-input" */).then(c => wrapFunctional(c.default || c))
+export const BaseNav = () => import('../../components/Navbar/BaseNav.vue' /* webpackChunkName: "components/base-nav" */).then(c => wrapFunctional(c.default || c))
+export const Content = () => import('../../components/Layout/Content.vue' /* webpackChunkName: "components/content" */).then(c => wrapFunctional(c.default || c))
+export const ContentFooter = () => import('../../components/Layout/ContentFooter.vue' /* webpackChunkName: "components/content-footer" */).then(c => wrapFunctional(c.default || c))
+export const DashboardLayout = () => import('../../components/Layout/DashboardLayout.vue' /* webpackChunkName: "components/dashboard-layout" */).then(c => wrapFunctional(c.default || c))
+export const DashboardNavbar = () => import('../../components/Layout/DashboardNavbar.vue' /* webpackChunkName: "components/dashboard-navbar" */).then(c => wrapFunctional(c.default || c))
+export const LoadingMainPanel = () => import('../../components/Layout/LoadingMainPanel.vue' /* webpackChunkName: "components/loading-main-panel" */).then(c => wrapFunctional(c.default || c))
+export const SidebarFixedToggleButton = () => import('../../components/Layout/SidebarFixedToggleButton.vue' /* webpackChunkName: "components/sidebar-fixed-toggle-button" */).then(c => wrapFunctional(c.default || c))
+export const SidebarSharePlugin = () => import('../../components/Layout/SidebarSharePlugin.vue' /* webpackChunkName: "components/sidebar-share-plugin" */).then(c => wrapFunctional(c.default || c))
+export const SidebarToggleButton = () => import('../../components/Layout/SidebarToggleButton.vue' /* webpackChunkName: "components/sidebar-toggle-button" */).then(c => wrapFunctional(c.default || c))
+export const SideBar = () => import('../../components/SidebarPlugin/SideBar.vue' /* webpackChunkName: "components/side-bar" */).then(c => wrapFunctional(c.default || c))
+export const SidebarItem = () => import('../../components/SidebarPlugin/SidebarItem.vue' /* webpackChunkName: "components/sidebar-item" */).then(c => wrapFunctional(c.default || c))
+export const SidebarPlugin = () => import('../../components/SidebarPlugin/index.js' /* webpackChunkName: "components/sidebar-plugin" */).then(c => wrapFunctional(c.default || c))
+export const Tab = () => import('../../components/Tabs/Tab.vue' /* webpackChunkName: "components/tab" */).then(c => wrapFunctional(c.default || c))
+export const Tabs = () => import('../../components/Tabs/Tabs.vue' /* webpackChunkName: "components/tabs" */).then(c => wrapFunctional(c.default || c))
+export const EditProfileForm = () => import('../../components/UserProfile/EditProfileForm.vue' /* webpackChunkName: "components/edit-profile-form" */).then(c => wrapFunctional(c.default || c))
+export const UserCard = () => import('../../components/UserProfile/UserCard.vue' /* webpackChunkName: "components/user-card" */).then(c => wrapFunctional(c.default || c))
+export const TimeLine = () => import('../../components/Timeline/TimeLine.vue' /* webpackChunkName: "components/time-line" */).then(c => wrapFunctional(c.default || c))
+export const TimeLineItem = () => import('../../components/Timeline/TimeLineItem.vue' /* webpackChunkName: "components/time-line-item" */).then(c => wrapFunctional(c.default || c))
+export const LoginForm = () => import('../../components/ValidationForms/LoginForm.vue' /* webpackChunkName: "components/login-form" */).then(c => wrapFunctional(c.default || c))
+export const RangeValidationForm = () => import('../../components/ValidationForms/RangeValidationForm.vue' /* webpackChunkName: "components/range-validation-form" */).then(c => wrapFunctional(c.default || c))
+export const RegisterForm = () => import('../../components/ValidationForms/RegisterForm.vue' /* webpackChunkName: "components/register-form" */).then(c => wrapFunctional(c.default || c))
+export const TypeValidationForm = () => import('../../components/ValidationForms/TypeValidationForm.vue' /* webpackChunkName: "components/type-validation-form" */).then(c => wrapFunctional(c.default || c))
+export const IotRespaldo = () => import('../../components/Widgets/IotRespaldo.vue' /* webpackChunkName: "components/iot-respaldo" */).then(c => wrapFunctional(c.default || c))
+export const Iotbutton = () => import('../../components/Widgets/Iotbutton.vue' /* webpackChunkName: "components/iotbutton" */).then(c => wrapFunctional(c.default || c))
+export const Iotcomplete = () => import('../../components/Widgets/Iotcomplete.vue' /* webpackChunkName: "components/iotcomplete" */).then(c => wrapFunctional(c.default || c))
+export const Iotindicator = () => import('../../components/Widgets/Iotindicator.vue' /* webpackChunkName: "components/iotindicator" */).then(c => wrapFunctional(c.default || c))
+export const Iotswitch = () => import('../../components/Widgets/Iotswitch.vue' /* webpackChunkName: "components/iotswitch" */).then(c => wrapFunctional(c.default || c))
+export const Rtnumberchart = () => import('../../components/Widgets/Rtnumberchart.vue' /* webpackChunkName: "components/rtnumberchart" */).then(c => wrapFunctional(c.default || c))
+export const FirstStep = () => import('../../components/Wizard/FirstStep.vue' /* webpackChunkName: "components/first-step" */).then(c => wrapFunctional(c.default || c))
+export const SecondStep = () => import('../../components/Wizard/SecondStep.vue' /* webpackChunkName: "components/second-step" */).then(c => wrapFunctional(c.default || c))
+export const ThirdStep = () => import('../../components/Wizard/ThirdStep.vue' /* webpackChunkName: "components/third-step" */).then(c => wrapFunctional(c.default || c))
+export const Wizard = () => import('../../components/Wizard/Wizard.vue' /* webpackChunkName: "components/wizard" */).then(c => wrapFunctional(c.default || c))
+export const WizardTab = () => import('../../components/Wizard/WizardTab.vue' /* webpackChunkName: "components/wizard-tab" */).then(c => wrapFunctional(c.default || c))
+export const Throttle = () => import('../../components/Wizard/throttle.js' /* webpackChunkName: "components/throttle" */).then(c => wrapFunctional(c.default || c))
+export const WorldMap = () => import('../../components/WorldMap/WorldMap.vue' /* webpackChunkName: "components/world-map" */).then(c => wrapFunctional(c.default || c))
+export const SampleFooter = () => import('../../components/Layout/starter/SampleFooter.vue' /* webpackChunkName: "components/sample-footer" */).then(c => wrapFunctional(c.default || c))
+export const SampleNavbar = () => import('../../components/Layout/starter/SampleNavbar.vue' /* webpackChunkName: "components/sample-navbar" */).then(c => wrapFunctional(c.default || c))
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
